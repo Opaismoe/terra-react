17eedff6f32f708eff1bf5f60d763b382d43a63c
Wiebe <wiebe.buursink@gmail.com>
2020-08-14 22:11:38 +0200

    fixed navigation and sidebar logic

--- src/components/NavbarMenu.js
+++ src/components/NavbarMenu.js
@@ -1,47 +1,87 @@
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import { Menu, Image, Button, Icon } from "semantic-ui-react";
 import brand from "../terralemon.svg";
-import './NavbarMenu.css'
+import "./NavbarMenu.css";
 
 const NavbarMenu = ({ isVisible, setIsVisible }) => {
-  const [active, setActive] = useState("home")
-  
+  const [activePath, setActivePath] = useState("home");
+
   const handleNavClick = (e) => {
-    setActive(e.target.href)
-  }
-  
+    let path = e.target.href.split("/").pop();
+    setActivePath(path);
+  };
+
   useEffect(() => {
-    
+    const setPath = () => {
+      setActivePath(window.location.pathname.split("/").pop())
+    }
+    setPath()
   })
 
   return (
-    <Menu style={style.navbar} borderless fixed="top">
+    <Menu style={style.navbar} borderless>
       <Menu.Item name="Home">
         <Link to="/">
           <Image src={brand} style={style.image} />
         </Link>
       </Menu.Item>
-      <Menu.Menu className="menu-right" position="right" style={style.rightMenu}>
-      <Menu.Item as={Link} to="/projecten" style={style.menuItem} name="projecten" active={active === "projecten"} onClick={() => handleNavClick}>
-        Projecten
-      </Menu.Item>
-      <Menu.Item as={Link} to="/cases" style={style.menuItem} name="cases" active={active === "cases"} onClick={() => handleNavClick}>
-        Cases
-      </Menu.Item>
-      <Menu.Item as={Link} to="/nieuws" style={style.menuItem} name="nieuws" active={active === "nieuws"} onClick={() => handleNavClick}>
-        Nieuws
-      </Menu.Item>
-      <Menu.Item as={Link} to="/over" style={style.menuItem} name="over" active={active === "over"} onClick={() => handleNavClick}>
-        Over Terralemon
-      </Menu.Item>
-        <Button style={style.menuButton} icon onClick={() => setIsVisible(!isVisible)}>
+      <Menu.Menu
+        className="menu-right"
+        position="right"
+        style={style.rightMenu}
+      >
+        <Menu.Item
+          as={Link}
+          to="/projecten"
+          style={style.menuItem}
+          name="projecten"
+          active={activePath === "projecten"}
+          onClick={handleNavClick}
+        >
+          Projecten
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/cases"
+          style={style.menuItem}
+          name="cases"
+          active={activePath === "cases"}
+          onClick={handleNavClick}
+        >
+          Cases
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/nieuws"
+          style={style.menuItem}
+          name="nieuws"
+          active={activePath === "nieuws"}
+          onClick={handleNavClick}
+        >
+          Nieuws
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/over"
+          style={style.menuItem}
+          name="over"
+          active={activePath === "over"}
+          onClick={handleNavClick}
+        >
+          Over Terralemon
+        </Menu.Item>
+        <Button
+          style={style.menuButton}
+          icon
+          onClick={() => setIsVisible(!isVisible)}
+        >
           <Icon name="bars" />
         </Button>
       </Menu.Menu>
     </Menu>
   );
-}
+};
 
 const style = {
   navbar: {
@@ -57,6 +97,6 @@ const style = {
     fontWeight: `bold`,
     textTransform: `uppercase`,
   },
-}
+};
 
 export default NavbarMenu;
--- src/components/SidebarMenu.js
+++ src/components/SidebarMenu.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom'
 import { Icon, Menu, Sidebar } from "semantic-ui-react";
+import "./SidebarMenu.css"
 
 const SidebarMenu = ({ isVisible, setIsVisible }) => {
+  const [activePath, setActivePath] = useState("home");
+
+  const handleNavClick = (e) => {
+    let path = e.target.href.split("/").pop();
+    setActivePath(path);
+    setIsVisible(!isVisible)
+  };
+
+  useEffect(() => {
+    const setPath = () => {
+      setActivePath(window.location.pathname.split("/").pop())
+    }
+    setPath()
+  })
+  
   return (
     <Sidebar
       as={Menu}
@@ -10,9 +26,8 @@ const SidebarMenu = ({ isVisible, setIsVisible }) => {
       onHide={() => setIsVisible(false)}
       visible={isVisible}
       size="massive"
-      direction="top"
+      direction="left"
       vertical
-      fluid
       borderless={true}
       fitted="horizontally"
     >
@@ -23,15 +38,46 @@ const SidebarMenu = ({ isVisible, setIsVisible }) => {
       >
         <Icon name="close" />
       </Menu.Item>
-      <Menu.Item as={Link} to="/projecten" style={style.menuItem}>
-        Projecten
-      </Menu.Item>
-      <Menu.Item as={Link} to="/nieuws" style={style.menuItem}>
-        Nieuws
-      </Menu.Item>
-      <Menu.Item as={Link} to="/over" style={style.menuItem}>
-        Over Terralemon
-      </Menu.Item>
+      <Menu.Item
+          as={Link}
+          to="/projecten"
+          style={style.menuItem}
+          name="projecten"
+          active={activePath === "projecten"}
+          onClick={handleNavClick}
+        >
+          Projecten
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/cases"
+          style={style.menuItem}
+          name="cases"
+          active={activePath === "cases"}
+          onClick={handleNavClick}
+        >
+          Cases
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/nieuws"
+          style={style.menuItem}
+          name="nieuws"
+          active={activePath === "nieuws"}
+          onClick={handleNavClick}
+        >
+          Nieuws
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/over"
+          style={style.menuItem}
+          name="over"
+          active={activePath === "over"}
+          onClick={handleNavClick}
+        >
+          Over Terralemon
+        </Menu.Item>
     </Sidebar>
   );
 };
