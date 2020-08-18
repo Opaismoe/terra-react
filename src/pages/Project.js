c5fba2fb2970f6e96f70a0cb443b2ae42746f0b5
Wiebe Buursink <wiebe@terralemon.nl>
2020-08-18 16:06:00 +0200

    added image gallery pkg, created image_gallery for fluid content

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Transition, Image } from "semantic-ui-react";
+import { Grid, Image, Container } from "semantic-ui-react";
 import { useParams, useHistory } from "react-router-dom"
 
 import AnimatedButton from "../components/AnimatedButton"
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import '../components/awesome-slider-custom.css'
 
 const Project = ({ projects, isLoading }) => {
   const { url_title } = useParams()
   const history = useHistory()
   const [oneProject, setOneProject] = useState([])
-
 
   useEffect(() => {
   const getOneProject = async () => {
@@ -22,27 +24,43 @@ const Project = ({ projects, isLoading }) => {
   
   return (
     <>
-      <Transition visible={!isLoading} animation='fade' duration={1450}>
         <Grid container centered>
           <Grid.Row style={style.row}>
             <AnimatedButton title="back" icon="long arrow alternate left" clickHandler={() => history.goBack()} style={{ marginLeft: 0, position: `absolute`, left: 0 }}/>
             {oneProject.map((project) => (
               <div key={project.id}>
-                <Image src={project.image} fluid/>
                 <h1 className="bigtitle" style={{ marginBottom: 0 }}>
                   {project.title}
                 </h1>
                 <p className ="lead">
                   {project.summary}
                 </p>
-                <h2>
-                  Case: {project.case.title}
-                </h2>
               </div>
             ))}
           </Grid.Row>
         </Grid>
-      </Transition>
+
+        <Container fluid>
+          <Grid.Row style={style.row}>
+          {oneProject.map((project) => (
+            <Image key={project.id} src={project.image} fluid/>
+          ))}
+          </Grid.Row>
+        </Container>
+
+        <Grid container centered>
+          <Grid.Row style={style.row}>
+            {oneProject.map((project) => (
+              project.fluid_content && project.fluid_content[0].image_gallery ?
+                <AwesomeSlider key={project.id} bullets={false}>
+                {project.fluid_content[0].image_gallery.map(src => (
+                  <img key={project.id} data-src={src} alt={src} />
+                ))}
+                </AwesomeSlider>
+                : null
+            ))}
+          </Grid.Row>
+        </Grid>
     </>
   );
 };
