9af3384acc9cd72cf1479067e753e6457a56fc9f
Wiebe Buursink <wiebe@terralemon.nl>
2020-08-20 10:50:58 +0200

    add he html encode package, add function to decode and inject html from api in project fluid content

--- src/components/project/ProjectFluidContent.js
+++ src/components/project/ProjectFluidContent.js
@@ -1,16 +1,22 @@
 import React from "react";
 
+import he from "he"
 import AwesomeSlider from "react-awesome-slider";
 import "react-awesome-slider/dist/styles.css";
 import "../../components/awesome-slider-custom.css";
 import { Header } from "semantic-ui-react";
 
 const ProjectFluidContent = ({ oneProject }) => {
+  const createMarkup = (content) => {
+    let data = he.decode(content)
+    return {__html: data}
+  }
+  
   return (
     <>
       {oneProject[0]?.fluid_content.map((content, i) =>
         content.content ? (
-          <p key={i}>{content.content}</p>
+          <div dangerouslySetInnerHTML={createMarkup(content.content)}></div>
         ) : content.image_project ? (
           <img
             key={i}
@@ -19,7 +25,7 @@ const ProjectFluidContent = ({ oneProject }) => {
           ></img>
         ) : content.subtitle ? (
           <Header key={i}>
-            <h4>{content.subtitle}</h4>
+            <h4>{he.decode(content.subtitle)}</h4>
           </Header>
         ) : content.image_gallery ? (
           <AwesomeSlider key={i} bullets={false}>
